1926cd11d9cf7d72ecaba45f7d0e5d6bca2df541
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2026-08-18 23:24:33 -0700

    refactor: alt implementation of state listener
    I wanted to try this different implementation to see if it would fix
    the bugs of pressing "a" too quickly upon entering a crafting table
    making "a" get stuck when exiting the crafting table. However, this was
    not the case, and the same bug is still present.

--- main.lua
+++ main.lua
@@ -33,17 +33,13 @@ local debug_text = "Press Shift + I to show keybinds.\n\n" ..
 return function(cfg, remaps)
     local keyboard_remaps = remaps.remapped_kb
     local other_remaps = remaps.normal_kb
+    local check_ingame = helpers.ingame_only(function() return true end)
     waywall.listen("state", function () 
-        local state = waywall.state()
-        if state.screen ~= "inworld" then
-            return
-        end
-        if state.inworld == "unpaused" then 
-            keyboard_remaps = remaps.remapped_kb
+        if check_ingame() then
+            waywall.set_remaps(remaps.remapped_kb)
         else
-            keyboard_remaps = remaps.menu_kb
+            waywall.set_remaps(remaps.menu_kb)
         end
-        waywall.set_remaps(keyboard_remaps)
     end)
 
     local config = {
