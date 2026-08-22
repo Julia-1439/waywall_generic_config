1e04eb32a02387d7cd34528f289f9f010ed9a08e
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2026-08-22 13:01:24 -0700

    fix: holding F3 preventing wide macro

--- init.lua
+++ init.lua
@@ -48,7 +48,7 @@ local cfg = {
     -- ==== MACROS ====
     -- resolution changes
     thin = { key = "*-Tab", f3_safe = false, ingame_only = true },
-    wide = { key = "*-Super_L", f3_safe = true, ingame_only = true },
+    wide = { key = "*-Super_L", f3_safe = false, ingame_only = true },
     tall = { key = "Control-B", f3_safe = false, ingame_only = false },
 
     -- startup actions
