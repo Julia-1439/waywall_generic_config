7e783620a7dc48bbe1690f8fd0f25ab8043d89a8
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2026-08-22 20:30:09 -0700

    chore: commenting extra behavior

--- extras.lua
+++ extras.lua
@@ -5,7 +5,9 @@ local remaps = require("remaps")
 local typing_in_menu = false
 local check_ingame = helpers.ingame_only(function() return true end) -- the anonymous function will return false if not ingame
 return function(config)
-    -- Add any extra code here
+
+    -- (*) Emulate A triggering Y when cursor-free 
+    -- (*)
     config.actions["*-A"] = function () 
         if not check_ingame() and (not typing_in_menu) then 
             waywall.press_key("Y")
@@ -15,17 +17,20 @@ return function(config)
         end
     end
 
+    -- (*)
     config.actions["*-M1"] = function ()
         typing_in_menu = false
         return false -- Pass the mouseclick to MC; do not consume
     end
 
+    -- (*)
     waywall.listen("state", function () 
         if check_ingame() then
             typing_in_menu = false
         end
     end)
 
+    -- Emulate W triggering A when cursor-free
     config.actions["Control-W"] = function ()
         if typing_in_menu then
             waywall.press_key("A")
@@ -34,6 +39,7 @@ return function(config)
         end
     end
 
+    -- Ninbot appears on F3+C
     config.actions["*-C"] = function()
         if waywall.get_key("F3") then
             waywall.show_floating(true)
@@ -42,6 +48,4 @@ return function(config)
             return false
         end
     end
-
-    -- END
 end
